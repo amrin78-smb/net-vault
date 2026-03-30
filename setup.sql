ec4d71d277638b51f0230261d773a99b06429e5c
amrin78-smb <amrin78@gmail.com>
2026-03-30 07:15:16 +0000

    fix: update default admin password hash to Admin1234!

--- setup.sql
+++ setup.sql
@@ -4,7 +4,7 @@ INSERT INTO users (name, email, password_hash, role)
 VALUES (
   'IT Admin',
   'admin@yourcompany.com',
-  '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4oZ9VLnTCu',
+  '$2b$12$QPxM2WDeRI1QO/u/5e17OOSG7Uy/fLB3Xwq.o/iU06IaP2.2PpFQ6',
   'admin'
 )
 ON CONFLICT (email) DO NOTHING;
